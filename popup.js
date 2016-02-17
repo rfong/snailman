dc7dfd7a1c93bbf12ed1bef80e3a24b41568d527
rfong <rachefo@gmail.com>
2016-02-16 20:21:53 -0800

    autodetect carriers

--- popup.js
+++ popup.js
@@ -1,4 +1,11 @@
+// Make underscore use Mustache style templating
+_.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
+};
+
+
 var storage = chrome.storage.local;
+
 
 var urls = {
   'USPS': "https://tools.usps.com/go/TrackConfirmAction_input?origTrackNum=%s",
@@ -6,30 +13,57 @@ var urls = {
   'FedEx': "https://www.fedex.com/fedextrack/?tracknumbers=%s",
   'Japan Post': "https://trackings.post.japanpost.jp/services/srv/search/direct?locale=en&reqCodeNo1=%s",
   'DHL': "http://webtrack.dhlglobalmail.com/?trackingnumber=%s",
-  };
+  'ELTA': "http://track.aftership.com/elta-courier/%s",
+  'Canada Post': 'http://www.canadapost.ca/cpotools/apps/track/personal/findByTrackNumber?trackingNumber=%s',
+};
+
+
+// slightly lenient regexes
+// todo strip whitespace before matching
+var serviceRegexes = {
+  'USPS': /^E\D{1}\d{9}$|^9\d{15,21}$/i,
+  'UPS': /^1Z[0-9A-Z]{0,16}$/i,
+  'FedEx': /^\d{12,14}$/i,
+  'DHL': /^\d{10}$/i,
+  'Canada Post': /^\d{16}$/i,
+  'Japan Post': /\d{9}JP$/i,
+  'ELTA': /\d{9}GR$/i,
+};
+
+
+function matchService(tracking) {
+  for (var service in serviceRegexes) {
+    if (tracking.match(serviceRegexes[service]))
+      return service;
+  } return null;
+}
+
 
 function getUrl(item) {
+  if (item.service == null) return;
   return urls[item.service].replace('%s', item.tracking);
 }
 
-function isDefaulted(selector) {
-  return $(selector).val() == $(selector).attr('data-default');
-}
 
 function updatePackages(packages) {
   storage.set({packages:packages});
   display();
 }
 
-// refresh content
+
 function display() {
   storage.get('packages', function(response) {
+    console.log(response.packages);
     $('#main').html('');
+
+    var linkTemplate = _.template('\
+      <a target="_blank" href="{{url}}" \
+         onClick="chrome.tabs.create({url:{{url}}})">\
+        {{service}} {{tracking}}</a>');
 
     _.each(response.packages, function(item, tracking) { 
         var url, el;
         url = getUrl(item);
-        if (url==null) return;
  
         el = $('<div class="item">\
             <div class="header"/>\
@@ -37,13 +71,12 @@ function display() {
           </div>');
         el.find('.header').html(
           (item.description ? '<b>'+item.description+'</b> - ' : '') +
-          '<a target="_blank" href="' + url + '" \
-              onClick="chrome.tabs.create({url:" + url + "})" >'
-            + item.service + ' ' + tracking +
-          '</a>' +
+          (item.service == null ?
+           tracking + ' (unidentified carrier)':
+           linkTemplate({url: url, service: item.service, tracking: tracking})
+          ) +
           '<i class="delete button icon-fixed-width icon-trash"></i>'
         );
-        //el.find('.info').html('info placeholder');
 
         el.find('.delete').click(function() {
           storage.get('packages', function(response) {
@@ -61,59 +94,25 @@ function display() {
 
 $(document).ready(function() {
 
-  // populate service dropdown
-  for (var service in urls)
-    $('select#service').append('<option value="'+service+'">'+service+'</option>');
-
-  // set up inputs with defaults
-  $('input[data-default]').each(function() {
-    var val = $(this).attr('data-default');
-    if (val) $(this).val(val).addClass('default');
-  })
-  .focusin(function() {
-    if ($(this).val() == $(this).attr('data-default'))
-      $(this).val('')
-        .removeClass('default');
-  })
-  .focusout(function() {
-    if ($(this).val().length==0)
-      $(this).val( $(this).attr('data-default') )
-        .addClass('default');
-  });
-
   display();
 
   var addHandler = function() {
-    var service = $('select#service').val(),
-        tracking = $('input#tracking').val().toString(),
+    var tracking = $('input#tracking').val().toString().replace(/ /g, ''),
+        service = matchService(tracking),
         description = $('input#description').val().toString();
-    // any fields still defaulted?
-    if (_.some( $('#add-package input'),
-        function(e) { return isDefaulted(e) } ))
-      return;
-    // reset to defaults
-    $('#add-package input[data-default]').each(function() {
-      $(this).val( $(this).attr('data-default') ).addClass('default');
-    });
 
     storage.get('packages', function(response) {
       packages = response.packages || {};
-
-      // already exists
-      if (_.some(packages, function(item,item_tracking){ item_tracking == tracking }))
-        return;
-
       packages[tracking] = {
         service: service,
         tracking: tracking,
         description: description,
         };
-      console.log(description, service, tracking);
       updatePackages(packages);
     });
   };
-  //$('#add').click(addHandler);
-  $('#add-package input').keypress( function(e){ if (e.which==13) addHandler() });
+  $('#add-package input').keypress(
+    function(e){ if (e.which==13) addHandler() });
 
 });
 
